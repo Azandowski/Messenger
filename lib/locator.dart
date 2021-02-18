4a262dc65424238324c6f484b122b918b64f2461
Yerkebulan <elzhanerke03@gmail.com>
2021-02-18 17:48:24 +0600

    init of service locator

---
+++ lib/locator.dart
@@ -0,0 +1,15 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async { 
+  
+  // local storage
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  // network online/offline mode
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
