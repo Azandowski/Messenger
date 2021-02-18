58e94f1659c452c8b85a55406f2640133196b9aa
Бекболат Азамат <azadev@MacBook-Pro-Bekbolat.local>
2021-02-18 18:05:18 +0600

    login events

--- lib/modules/authentication/bloc/authentication_event.dart
+++ lib/modules/authentication/bloc/authentication_event.dart
@@ -1,8 +1,19 @@
-part of 'authentication_bloc.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class AuthenticationEvent extends Equatable {
-  const AuthenticationEvent();
-
   @override
   List<Object> get props => [];
 }
+
+class AppStarted extends AuthenticationEvent {}
+
+class LoggedIn extends AuthenticationEvent {
+  final String token;
+
+  LoggedIn(this.token);
+
+  @override
+  List<Object> get props => [token];
+}
+
+class LoggedOut extends AuthenticationEvent {}
