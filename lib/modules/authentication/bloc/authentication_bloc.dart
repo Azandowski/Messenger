aaef10a788ddf5629372ae9109f1aa594d871ec2
Бекболат Азамат <azadev@MacBook-Pro-Bekbolat.local>
2021-02-18 19:01:13 +0600

    getTOken usecase added

--- lib/modules/authentication/bloc/authentication_bloc.dart
+++ lib/modules/authentication/bloc/authentication_bloc.dart
@@ -1,18 +1,58 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:messenger_mobile/core/error/failures.dart';
+import 'package:messenger_mobile/core/usecases/usecase.dart';
+import 'package:messenger_mobile/modules/authentication/domain/usecases/get_token.dart';
+import '../../../core/config/storage.dart';
+import 'authentication_event.dart';
+import 'authentication_state.dart';
 
-part 'authentication_event.dart';
-part 'authentication_state.dart';
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final GetToken getToken;
 
-class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationInitial());
+  AuthenticationBloc({
+    @required this.getToken,
+  }) : super(Uninitialized());
 
   @override
   Stream<AuthenticationState> mapEventToState(
     AuthenticationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    // app start
+    if (event is AppStarted) {
+      var token = await getToken(NoParams());
+      token.fold((failure) => Error(message: _mapFailureToMessage(failure)),
+          (token) async* {
+        if (token != '') {
+          Storage().token = token;
+          yield Authenticated();
+        } else {
+          yield Unauthenticated();
+        }
+      });
+    }
+
+    // if (event is LoggedIn) {
+    //   Storage().token = event.token;
+    //   await _saveToken(event.token);
+    //   yield Authenticated();
+    // }
+
+    // if (event is LoggedOut) {
+    //   Storage().token = '';
+    //   await _deleteToken();
+    //   yield Unauthenticated();
+    // }
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return 'SERVER_FAILURE_MESSAGE';
+      default:
+        return 'Unexpected error';
+    }
   }
 }
---
+++ lib/modules/authentication/data/repositories/authentication_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:messenger_mobile/core/network/network_info.dart';
+import 'package:messenger_mobile/modules/authentication/data/datasources/local_authentication_datasource.dart';
+import 'package:messenger_mobile/modules/authentication/data/datasources/remote_authentication_datasource.dart';
+import 'package:messenger_mobile/modules/authentication/domain/repositories/authentication_repository.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/error/failures.dart';
+
+class AuthenticationRepositiryImpl implements AuthenticationRepository {
+  final AuthenticationRemoteDataSource remoteDataSource;
+  final AuthenticationLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  AuthenticationRepositiryImpl({
+    @required this.remoteDataSource,
+    @required this.networkInfo,
+    @required this.localDataSource,
+  });
+
+  @override
+  Future<Either<Failure, String>> sendPhone(String phoneNumber) async {
+    // TODO: implement sendPhone
+    throw UnimplementedError();
+  }
+}
